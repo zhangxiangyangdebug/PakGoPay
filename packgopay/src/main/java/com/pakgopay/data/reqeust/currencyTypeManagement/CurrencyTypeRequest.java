6097049dfddd9bdeee453cf9ee97ac0bbb932f7a
zf <574676312@qq.com>
2026-01-30 14:20:56 +0800

    币种查询分页

--- packgopay/src/main/java/com/pakgopay/data/reqeust/currencyTypeManagement/CurrencyTypeRequest.java
+++ packgopay/src/main/java/com/pakgopay/data/reqeust/currencyTypeManagement/CurrencyTypeRequest.java
@@ -7,10 +7,16 @@ import java.math.BigDecimal;
 
 @Data
 public class CurrencyTypeRequest extends BaseRequest {
+    private Integer id;
     private String name;
     private String currencyType;
     private String icon;
     private BigDecimal currencyAccuracy;
     private Integer isRate;
     private BigDecimal exchangeRate;
+    private Integer pageNo;
+    private Integer pageSize;
+    public Integer getOffSet() {
+        return (pageNo - 1) * pageSize;
+    }
 }
--- packgopay/src/main/java/com/pakgopay/mapper/CurrencyTypeMapper.java
+++ packgopay/src/main/java/com/pakgopay/mapper/CurrencyTypeMapper.java
@@ -1,15 +1,17 @@
 package com.pakgopay.mapper;
 
+import com.pakgopay.data.reqeust.currencyTypeManagement.CurrencyTypeRequest;
 import com.pakgopay.mapper.dto.CurrencyTypeDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface CurrencyTypeMapper {
-    List<CurrencyTypeDTO> getAllCurrencyType();
+    List<CurrencyTypeDTO> getAllCurrencyType(CurrencyTypeRequest currencyTypeRequest);
 
-    List<CurrencyTypeDTO> getCurrencyById(Integer id);
+    Integer getCount( CurrencyTypeRequest entity);
 
     Integer addNewCurrency(CurrencyTypeDTO currencyTypeDTO);
 }
